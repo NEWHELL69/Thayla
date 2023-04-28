d7c7d7dcf513d87902c1601f28eb95a3f926fd06
NEWHELL69 <kushagra0304@gmail.com>
2023-04-29 02:34:33 +0530

    menu layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = config => {
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
+
+  config.addWatchTarget("./src/js/");
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
---
+++ src/js/site-head.js
@@ -0,0 +1,7 @@
+// This script should be added in the html document after the styles of the element it uses.
+
+// (function(){
+//     const root = document.querySelector(":root");
+//     const menuMain = document.querySelector(".menu .menu__main");
+//     root.style.setProperty("--menu-main-width", `-${menuMain.offsetWidth}px`)
+// })();         
